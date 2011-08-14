f632768a81de8471d135ad5e237d9ddcda249928
pcfighter <pblaszkow5@gmail.com>
2011-08-14 21:23:51 +0200

    cleaned after mur4ik's build

--- .tmp_versions/pmem_kernel_test.mod
+++
@@ -1,2 +0,0 @@
-drivers/misc/pmem_kernel_test.ko
-drivers/misc/pmem_kernel_test.o
--- include/linux/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 13 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#13 PREEMPT Tue May 31 06:20:48 EEST 2011"
-#define LINUX_COMPILE_TIME "06:20:48"
-#define LINUX_COMPILE_BY "lg-electronics"
-#define LINUX_COMPILE_HOST "mur4ik-VirtualBox"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
